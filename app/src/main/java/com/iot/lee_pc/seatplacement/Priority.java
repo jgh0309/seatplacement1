67708674f50d3c6673842628cf35ca02219082e8
pack <qedwjs12@naver.com>
2017-05-24 10:20:27 +0900

    2.버튼 우선순위 연동 수정

--- app/src/main/java/com/iot/lee_pc/seatplacement/Priority.java
+++ app/src/main/java/com/iot/lee_pc/seatplacement/Priority.java
@@ -18,26 +18,25 @@ public class Priority extends AppCompatActivity {
 
         int _idnum = 0;
         int max = 0;
-        //   String studentnum = "select stu_seat"+(k+1)+" from "+ _idnum;
-        //Map<String,String>map = new HashMap<String,String>();
+
         createDatabase("Student");
 
-        String SQL = " select stu_no from  StudentInformation";//레코드 개수 찾기
+        String SQL = " select _id from  StudentInformation";//레코드 개수 찾기
         Cursor count = db.rawQuery(SQL,null);
         int recordCount = count.getCount();
 
 
 
-        for (int seatnum = 0; seatnum < recordCount; seatnum++) { //지망 개수
+        for (int seatnum = 0; seatnum < 3; seatnum++) { //지망 개수
 
-            for (int i = 0; i < 5; i++) { //비교할 인원
-                //_idnum = 아이디
+            for (int i = 0; i < recordCount-1; i++) { //비교할 인원
+                //(i+1) = 아이디
                 //stu_seat seatnum = 순위좌석 번호
                 //money_seatnum = 순위좌석 경매가격
                 //seat_final = 최종 확인 좌석
 
 
-                String stu1 = " select " + _idnum + ",  stu_seat" + seatnum + ", money_tender" + seatnum + ", seat_final from StudentInformation";
+                String stu1 = " select _id,  stu_seat" + seatnum + ", money_tender" + seatnum + ", SeatPlace from StudentInformation  where id="+i;
                 Cursor c1 = db.rawQuery(stu1, null);
 
                 c1.moveToNext();
@@ -47,8 +46,8 @@ public class Priority extends AppCompatActivity {
                 String stu_set_seat = c1.getString(3);
 
                 if (Integer.parseInt(stu_set_seat) == 0) { //확정 자리가 비어있으면
-                    for (int j = i + 1; j < 6; j++) {
-                        String stu2 = " select " + _idnum + ",  stu_seat" + seatnum + ", money_tender" + seatnum + ", seat_final from StudentInformation";
+                    for (int j = i + 1; j < recordCount; j++) {
+                        String stu2 = " select _id ,  preference" + seatnum + ", money_tender" + seatnum + ", SeatPlace from StudentInformation where id="+j ;
                         Cursor c2 = db.rawQuery(stu2, null);
              //           int recordCount_compare = c2.getCount();
 
@@ -58,19 +57,20 @@ public class Priority extends AppCompatActivity {
                         String stu_money_compare = c2.getString(2);
                //         String stu_set_seat_compare = c2.getString(2);
 
+
                         if (stu_seat.equals(stu_seat_compare)) { //선택한 자리가 같을 시
                             int _seat_money = Integer.parseInt(stu_money); //비교 하는 사람의 입찰금을 저장
                             int _seat_money_compare = Integer.parseInt(stu_money_compare); //비교 당하는 사람의 입찰금을 저장
 
                             if (_seat_money > _seat_money_compare) { //금액이 더욱 크면 확정 자리에 금액을 넣음
 
-                                db.execSQL("update StudentInformation set seat_final = " + stu_seat + " where stu_name = " + stu_no);
+                                db.execSQL("update StudentInformation set SeatPlace = " + stu_seat + " where name = " + stu_no);
                             } else if (_seat_money < _seat_money_compare && max < _seat_money_compare) { //작으면 다른 사람의 금액을 넣음 ,max보다 큰 값이 들어가야 들어감
                                 max = _seat_money_compare;
-                                db.execSQL("update StudentInformation set seat_final= " + stu_seat_compare + " where stu_name = " + stu_no_compare);
+                                db.execSQL("update StudentInformation set SeatPlace= " + stu_seat_compare + " where _id = "+ stu_no_compare);
                             }
                         } else {//선택한 자리가 다를 시 최종 자리 seat_final 에 자리번호가 들어간다
-                            db.execSQL("update StudentInformation set seat_final = " + stu_seat + " where stu_name = " + stu_no);
+                            db.execSQL("update StudentInformation set SeatPlace = " + stu_seat + " where _id = " + stu_no);
 
                         }
 
