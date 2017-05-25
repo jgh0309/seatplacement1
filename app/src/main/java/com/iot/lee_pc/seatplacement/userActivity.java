a3f7855d14e1bb352c21addff322d315b80d055e
laby <labyrin93!>
2017-05-25 17:14:40 +0900

    입력하지 않은 항목이 있을 시 입력 안됨

--- app/src/main/java/com/iot/lee_pc/seatplacement/userActivity.java
+++ app/src/main/java/com/iot/lee_pc/seatplacement/userActivity.java
@@ -16,7 +16,7 @@ public class userActivity extends AppCompatActivity
     SQLiteDatabase db;
     int totalseat;
     int totalman;
-    int totalcount=0;
+    int totalcount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -32,41 +32,48 @@ public class userActivity extends AppCompatActivity
         final EditText edit1st_batting = (EditText) findViewById(R.id.edit1st_batting);
         final EditText edit2nd_batting = (EditText) findViewById(R.id.edit2nd_batting);
         final EditText edit3rd_batting = (EditText) findViewById(R.id.edit3rd_batting);
-        TextView textView = (TextView) findViewById(R.id.textView3);
+        final TextView textView = (TextView) findViewById(R.id.textView3);
 
         Intent intent = getIntent();
         Content total = (Content) intent.getSerializableExtra("total");
         totalseat = total.getTotalseat();
         totalman = total.getTotalman();
-        textView.setText("총 "+totalseat+" 개의 좌석이 있습니다.");
+        textView.setText("총 " + totalseat + " 개의 좌석이 있습니다.");
 
         final Button button = (Button) findViewById(R.id.nextBTN);
         button.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
-            {
-                final int preference1 = Integer.parseInt(edit1st_preference.getText().toString());
-                final int preference2 = Integer.parseInt(edit1st_preference.getText().toString());
-                final int preference3 = Integer.parseInt(edit1st_preference.getText().toString());
-                if((preference1<=totalseat) && (preference2<=totalseat) && (preference3<=totalseat))
-                {
-                    insertStudentInfo(editName.getText().toString(), edit1st_preference.getText().toString(), edit2nd_preference.getText().toString(),
-                            edit3rd_preference.getText().toString(), edit1st_batting.getText().toString(), edit2nd_batting.getText().toString(), edit3rd_batting.getText().toString());
-                    editName.setText("");
-                    edit1st_preference.setText("");
-                    edit2nd_preference.setText("");
-                    edit3rd_preference.setText("");
-                    edit1st_batting.setText("");
-                    edit2nd_batting.setText("");
-                    edit3rd_batting.setText("");
-                    totalcount++;
-                }
-                else Toast.makeText(getApplicationContext(),"최대 좌석수 이하로 선택하세요.",Toast.LENGTH_SHORT).show();
+            {   //edittext에 null이 있는지 확인
+                if(editName.getText().toString().equals("")||edit1st_preference.getText().toString().equals("")||edit2nd_preference.getText().toString().equals("")
+                        ||edit3rd_preference.getText().toString().equals("") ||edit1st_batting.getText().toString().equals("")||edit2nd_batting.getText().toString().equals("") ||edit3rd_batting.getText().toString().equals(""))
+                    Toast.makeText(getApplicationContext(), "입력하지 않은 항목이 있습니다.", Toast.LENGTH_SHORT).show();
+                else {
+                    final int preference1 = Integer.parseInt(edit1st_preference.getText().toString());
+                    final int preference2 = Integer.parseInt(edit1st_preference.getText().toString());
+                    final int preference3 = Integer.parseInt(edit1st_preference.getText().toString());
+                    if ((preference1 <= totalseat) && (preference2 <= totalseat) && (preference3 <= totalseat))
+                    {
+                        insertStudentInfo(editName.getText().toString(), edit1st_preference.getText().toString(), edit2nd_preference.getText().toString(),
+                                edit3rd_preference.getText().toString(), edit1st_batting.getText().toString(), edit2nd_batting.getText().toString(), edit3rd_batting.getText().toString());
+                        editName.setText("");
+                        edit1st_preference.setText("");
+                        edit2nd_preference.setText("");
+                        edit3rd_preference.setText("");
+                        edit1st_batting.setText("");
+                        edit2nd_batting.setText("");
+                        edit3rd_batting.setText("");
+                        totalcount++;
+                        textView.setText("총 " + (totalseat-totalcount) + " 개의 좌석이 있습니다.");
+                    } else
+                        Toast.makeText(getApplicationContext(), "최대 좌석수 이하로 선택하세요.", Toast.LENGTH_SHORT).show();
 
-                if(totalcount == totalman){
-                    Toast.makeText(getApplicationContext(),"참여인원 모두가 선택하였습니다."+"\n"+"결과보기 버튼을 클릭해주세요.",Toast.LENGTH_SHORT).show();
-                    button.setVisibility(View.INVISIBLE);
+                    if (totalcount == totalman)
+                    {
+                        Toast.makeText(getApplicationContext(), "참여인원 모두가 선택하였습니다.\n결과보기 버튼을 클릭해주세요.", Toast.LENGTH_SHORT).show();
+                        button.setVisibility(View.INVISIBLE);
+                    }
                 }
             }
         });
@@ -77,11 +84,16 @@ public class userActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                Intent intent = new Intent(
-                        getApplicationContext(),
-                        resultActivity.class
-                );
-                startActivity(intent);
+                if (totalcount != totalman)
+                    Toast.makeText(getApplicationContext(), "아직 모든 인원이 선택하지 않았습니다.\n모두 참여할 때까지 기다려주세요.", Toast.LENGTH_SHORT).show();
+                else
+                {
+                    Intent intent = new Intent(
+                            getApplicationContext(),
+                            resultActivity.class
+                    );
+                    startActivity(intent);
+                }
             }
         });
 
@@ -91,6 +103,7 @@ public class userActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
+
                 Intent intent = new Intent(
                         getApplicationContext(),
                         drawingRect.class
@@ -100,15 +113,17 @@ public class userActivity extends AppCompatActivity
         });
     }
 
-    public void insertStudentInfo(String editName ,String edit1st_preference ,String edit2nd_preference ,String edit3rd_preference ,
-                                  String edit1st_batting, String edit2nd_batting, String edit3rd_batting){
-        try{
+    public void insertStudentInfo(String editName, String edit1st_preference, String edit2nd_preference, String edit3rd_preference,
+                                  String edit1st_batting, String edit2nd_batting, String edit3rd_batting)
+    {
+        try
+        {
             db.execSQL(
 
                     "insert into StudentInformation" +
                             " (name, preference1, preference2, preference3, batting1, batting2, batting3, SeatPlace)" +
-                            " values ( '" + editName +"', "+ edit1st_preference +", "+ edit2nd_preference +", "+ edit3rd_preference +
-                            ", "+ edit1st_batting +", "+ edit2nd_batting +", "+ edit3rd_batting +", 100 );"
+                            " values ( '" + editName + "', " + edit1st_preference + ", " + edit2nd_preference + ", " + edit3rd_preference +
+                            ", " + edit1st_batting + ", " + edit2nd_batting + ", " + edit3rd_batting + ", 100 );"
                     /*"insert into StudentInformation" +
                             " (name, preference1, preference2, preference3, batting1, batting2, batting3, SeatPlace)" +
                             " values ( 'PARK', 3, 1, 2, 4500, 2000, 3000, 100 );");
@@ -132,22 +147,24 @@ public class userActivity extends AppCompatActivity
                     "insert into StudentInformation" +
                             " (name, preference1, preference2, preference3, batting1, batting2, batting3, SeatPlace)" +
                             " values ( 'LEE', 1, 6, 2, 3500, 2000, 3000, 100 );"*/);
-            Toast.makeText(getApplicationContext(), "삽입성공" , Toast.LENGTH_SHORT).show();
-        }catch(Exception e)
+            Toast.makeText(getApplicationContext(), "삽입성공", Toast.LENGTH_SHORT).show();
+        } catch (Exception e)
         {
             e.printStackTrace();
             Toast.makeText(getApplicationContext(), "삽입실패", Toast.LENGTH_SHORT).show();
         }
     }
 
-    public  void createDatabase(String dbName){ //db생성
-        try {  //오류검출시도
+    public void createDatabase(String dbName)
+    { //db생성
+        try
+        {  //오류검출시도
             db = openOrCreateDatabase(
                     dbName,
                     Activity.MODE_PRIVATE,
                     null);
-        }
-        catch(Exception e){ //오류검출
+        } catch (Exception e)
+        { //오류검출
             e.printStackTrace();
         }
 
