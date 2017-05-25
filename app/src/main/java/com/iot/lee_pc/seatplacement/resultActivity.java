107e11c741a1901c7609126f27e22b885247f112
pack <qedwjs12@naver.com>
2017-05-25 18:52:02 +0900

    2.자리배치 , 랜덤배치 완료

--- app/src/main/java/com/iot/lee_pc/seatplacement/resultActivity.java
+++ app/src/main/java/com/iot/lee_pc/seatplacement/resultActivity.java
@@ -93,47 +93,55 @@ public class resultActivity extends AppCompatActivity {
                 c1.close();
             }
             if (preferences == 3) {
-                int temp = 0;
-                int seatnumber;
-                int a[] = new int[recordCount]; //int형 배열 선언 빈자리의 a개수
+                // randomSeat(recordCount);
+////////////////////////////////////////////////////
+
+                       String Record = "select _id from  StudentInformation where  SeatPlace = 100";//레코드 개수 찾기
+                      Cursor _count = db.rawQuery(Record, null);
+                     int nullseatCount = _count.getCount();  //빈자리 개수
+               int a[] = new int[nullseatCount]; //int형 배열 선언 빈자리의 a개수
+                //     _count.close();
                 int b[] = new int[recordCount];
-                for (int i = 0; i < recordCount; i++) {
-                    a[i] = (int) (Math.random() * recordCount + 1);
-                    for (int j = 0; j < i; j++) //중복제거를 위한 for문
-                    {
 
-                        if (a[i] == a[j]) {
-
-                            i--;
-                            break;
-                        }
-                    }
-                }
                 Cursor saveCursor = db.rawQuery("select SeatPlace from StudentInformation ", null);
                 for(int i = 0 ; i < recordCount ; i ++ )
                 {
                     saveCursor.moveToNext();
                     b[i]=saveCursor.getInt(0);
                 }
+                for (int i = 0; i < nullseatCount; i++) {
+                    a[i] = (int) (Math.random() * recordCount + 1);
+                    for (int j = 0; j < recordCount; j++) //중복제거를 위한 for문
+                    {
+
+                        if (a[i] == b[j]) {
+
+                            i--;
+                            break;
+                        }
+                    }
+                }
+
+
                 for (int i = 0; i < recordCount; i++) { //랜덤
+//i가 되는 자리가 비었을때 100 , 일 대 랜덤의 배열을 하나씩 검사하며 같은 것이 없으면 넣는다
+                    for (int j = 0; j < nullseatCount; j++) { //사용자
 
-                    for (int j = 0; j < recordCount; j++) { //사용자
-                        if (a[i] == b[j]) { //랜덤값과 자리값이 같으면 멈추고 다음 랜덤자리 시작
-                            break;
+                        if (100 == b[i]) { //자리가 비어있으면
+                            db.execSQL("update StudentInformation set SeatPlace = " + a[j] + " where _id=" + (i+1));
                         }
-                        if (100 == b[j]) { //자리가 비어있으면 저장 을하고
-                            temp = j + 1; //비어있는 자리의 위치
-
-                        }
-                        if (j == recordCount - 1) // 랜덤 자리 시작이 되지않고 넘어오면 마지막에 업데이트
+                        else
                         {
-                            db.execSQL("update StudentInformation set SeatPlace " + "= " + a[i] + " where _id=" + temp);
                             break;
                         }
+
 
                     }
                 }
+
+
             }
+
         }
     }
 
@@ -177,7 +185,12 @@ public class resultActivity extends AppCompatActivity {
         }
         return infos;
     }
-}
+
+
+
+
+
+    }
 
 
 
